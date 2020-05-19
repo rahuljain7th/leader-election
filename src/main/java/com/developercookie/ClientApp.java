27410edfb59c7821fc9bfeb9f1b899a82c47257c
Rahul <jain_rahul@ymail.com>
2020-05-18 22:09:50 -0400

    feature:2-Zookeeper Connection -adding comments

--- src/main/java/com/developercookie/ClientApp.java
+++ src/main/java/com/developercookie/ClientApp.java
@@ -8,6 +8,9 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
+/**
+ * The type Client app which Connects to Zookeeper.
+ */
 public class ClientApp implements Watcher {
     private static final Logger logger = LoggerFactory.getLogger(ClientApp.class);
     // The Address in which Zookeeper is Started
@@ -18,6 +21,13 @@ public class ClientApp implements Watcher {
 
     private static final int Session_Timeout = 3000;
 
+    /**
+     * The entry point of Client application.
+     *
+     * @param args the input arguments
+     * @throws IOException          the io exception
+     * @throws InterruptedException the interrupted exception
+     */
     public static void main(String[] args) throws IOException, InterruptedException {
         ClientApp clientApp = new ClientApp();
         clientApp.connectToZookeeper();
@@ -26,22 +36,38 @@ public class ClientApp implements Watcher {
         logger.warn("Successfully Disconnect from Zookeeper");
     }
 
+    /**
+     * Connect to zookeeper.Creating the Zookeeper Object to Communicate with Zookeeper
+     *
+     * @throws IOException the io exception
+     */
     public void connectToZookeeper() throws IOException {
         this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, Session_Timeout, this);
 
     }
 
+    /**
+     * Closing the Zookeeper Connection.
+     *
+     * @throws InterruptedException the interrupted exception
+     */
+// Closing
     public void close() throws InterruptedException {
         zooKeeper.close();
     }
 
+    /**
+     * Wait till the Zookeeper Event Thread is kept open .
+     *
+     * @throws InterruptedException the interrupted exception
+     */
     public void run() throws InterruptedException {
         synchronized (zooKeeper) {
             zooKeeper.wait();
         }
     }
 
-    // The Process Method will be Called by Zookeeper Library in a Seperate Event Thread.
+    // The Process Method will be Called by Zookeeper Library in a Separate Event Thread.
     @Override
     public void process(WatchedEvent event) {
         switch (event.getType()) {
